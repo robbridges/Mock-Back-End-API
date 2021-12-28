b7b1a416208a83720db0f9831d5f30c4b0dcdeb3
robbridges <robbridges@users.noreply.github.com>
2021-12-28 14:17:18 -0800

    Basics of second route up, needs more work.

--- server/index.js
+++ server/index.js
@@ -1,12 +1,35 @@
 const express = require('express');
 
+const axios  = require('axios');
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+// const getMockData = async () => {
+//   const response = await axios.get('https://api.hatchways.io/assessment/blog/posts?tag=tech');
+//   console.log(response.data);
+// } 
+
+// getMockData();
+
 app.get('/api/ping', (req, res) => {
   res.status(200).send({"success": true});
-})
+});
+
+app.get('/api/posts', async (req,res) => {
+  if (!req.query.tags) {
+    return res.status(400).send({"error": "Tags parameter is required"});
+  }
+  try {
+    const {data} = await axios.get(`https://api.hatchways.io/assessment/blog/posts?tag=${req.query.tags}`);
+    return res.status(200).send(data);
+  } catch (e) {
+    return res.status(500).send(e.message);
+  }
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT} `)
