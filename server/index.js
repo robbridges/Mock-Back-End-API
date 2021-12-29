b0744fe7f10ec1d66293f28d71097ff22b608e42
robbridges <robbridges@users.noreply.github.com>
2021-12-29 10:44:36 -0800

    Sorting isn't working into proper order, other than that second route is up.

--- server/index.js
+++ server/index.js
@@ -12,11 +12,25 @@ app.get('/api/ping', (req, res) => {
 });
 
 app.get('/api/posts', async (req,res) => {
+  // these are the only 3 that directions can be, it took me a minute to think of including undefeined in the event the client doesn't provide it.
+  const directions = ['asc', 'desc', undefined];
+  // the only options that should be passed to sort, we'll throw another error if it's not included in our array
+  const sortKeywords = ['popularity', 'likes', 'reads', 'id', undefined];
+
+  if (!directions.includes(req.query.direction)) {
+    return res.status(400).send({error: "Direction parameter must be asc or desc"});
+  }
+
+  if (!sortKeywords.includes(req.query.sortBy)) {
+    return res.status(400).send({error: "sortBy Parameter is invalid, please choose between popularity, likes, reads, or id"})
+  }
+
   if (!req.query.tags) {
     return res.status(400).send({"error": "Tags parameter is required"});
   }
-  // if the user gives a comma seperated tag, we need to handle it with concurrent api requests.
-  if (req.query.tags.includes(',')) {
+    /* if the user gives a comma seperated tag, we need to handle it with concurrent api requests, I removed the else catch if one tag was supplied. 
+    we're actually taking of everything in this one. */
+  
     const tagArray = req.query.tags.split(',');
     
     let posts = [];
@@ -37,22 +51,15 @@ app.get('/api/posts', async (req,res) => {
     
     // if the query param desc is passed we sort the posts the ports descending based on the field in the query param.
     if (req.query.direction === 'desc') {
-      posts = posts.sort((a, b) => (b[req.query.sortBy] < a[req.query.sortBy]) ? -1 : 1)
+      posts = posts.sort((a, b) => (b[req.query.sortBy] < a[req.query.sortBy]) ? 1 : -1)
     } else {
       posts = posts.sort((a, b) => (b[req.query.sortBy] < a[req.query.sortBy]) ? 1 : -1)
     }
 
     return res.status(200).send({posts});
-  // user has only given one tag, we can handle it as we normally would.   
-  } else {
-    try {
-      const {data} = await axios.get(`https://api.hatchways.io/assessment/blog/posts?tag=${req.query.tags}`);
-      return res.status(200).send(data);
-    } catch (e) {
-      return res.status(500).send(e.message);
-    }
-  } 
 });
+
+
 // we need to compare each object within the posts, the best way to compare an object is Lodash, I've fought and argued about trying to not use a nested loop, but for now it'll have to do.
 const checkforDuplicatePosts =(existingPosts, newPosts) => {
   for (let i = 0; i < newPosts.length; i++) {
@@ -72,11 +79,6 @@ const checkforDuplicatePosts =(existingPosts, newPosts) => {
 
   return existingPosts;
 }
-
-
-
-
-
 
 
 app.listen(PORT, () => {
