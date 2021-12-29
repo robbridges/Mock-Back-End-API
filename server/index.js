9df29b4499a11d088f9112461e8cd3f1529f3e58
robbridges <robbridges@users.noreply.github.com>
2021-12-28 17:11:57 -0800

    Requests handled concurrently

--- server/index.js
+++ server/index.js
@@ -25,14 +25,22 @@ app.get('/api/posts', async (req,res) => {
   // if the user gives a comma seperated tag, we need to handle it with concurrent api requests.
   if (req.query.tags.includes(',')) {
     const tagArray = req.query.tags.split(',');
+    
     const posts = [];
+    // EUREKA!! Concurrently handled them all by mapping axios requests
+    const axiosRequests = tagArray.map((tag) =>
+      axios.get(`https://api.hatchways.io/assessment/blog/posts?tag=${tag}` )
+    );
 
-
-    // this works.. kind of.. it has two whole array listings of posts, and I know that there's got to be a better way. Work on this. 
-    for (let i = 0; i < tagArray.length; i++) {
-      const {data} = await axios.get(`https://api.hatchways.io/assessment/blog/posts?tag=${tagArray[i]}`);
-      posts.push(data);
+    try {
+      const axiosResults = await Promise.all(axiosRequests);
+      axiosResults.map((result) => {
+        posts.push(result.data.posts);
+      })
+    } catch (err) {
+      res.status(500).send(e.message);
     }
+    
     return res.status(200).send({posts});
   // user has only given one tag, we can handle it as we normally would.   
   } else {
